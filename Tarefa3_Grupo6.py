ff1287053059ebb931a7c3e474e3c261ee6302d0
jmbessa <54651955+jmbessa@users.noreply.github.com>
2021-06-03 17:40:23 -0300

    Add files via upload

---
+++ Tarefa3_Grupo6.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 28 15:18:25 2021
+
+@author: jujub_000
+"""
+
+# Turma:33D
+# Professor:JOISA
+# Nome completo:Juliana Rezende Coutinho
+# Matrícula PUC-Rio:1810391
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.set_option("display.max_rows",100)
+
+dFilmes=pd.read_excel("Grupo6_excel.xlsx",header=0)
+
+print(dFilmes)
+
+#Para melhorar a visualizaçÃo do Data Frame eliminaremos a coluna que contém a sinopse do filme
+print('\n DFilmes sem timeline')
+dFilmes.drop(["timeline","Column1"],axis=1,inplace=True)
+print(dFilmes)
+
+#Substituição de valores
+print('\n1 DFilmes com colunas renomeadas em português')
+dFilmes.rename(columns={"name":"Nome","year":"Ano","runtime":"Duração","genre": "Gênero","rating":"Avaliação","metascore":"Nota","votes":"Votos","gross":"Faturamento"},inplace=True)
+print(dFilmes)
+
+print('\n - Preenchendo valores distintos na Nota e Faturamento \
+      Em nota com a media da nota dos filmes e em Faturamento com a moda de faturamento')
+media = ( '%.f'%dFilmes.Nota.mean())
+dFilmes.fillna({'Nota':media,'Faturamento':dFilmes.Faturamento.mode().loc[0]}, inplace=True)
+print(dFilmes)
+
+#Filtros
+# print('\n2 Todos os filmes de apenas Drama')
+# dfDrama=dFilmes.loc[dFilmes.Gênero=="Drama"]
+# print(dfDrama)
+
+print('\n3- Nome do(s) filme(s) de maior duração')
+dfMaiorTempo=dFilmes.loc[dFilmes.Duração==dFilmes.Duração.max()]
+print(dfMaiorTempo.Nome.values,"- Tempo de duração:",dFilmes.Duração.max(),"min")
+
+
+print('\n4- Df dos filmes de avaliação maior ou igual a 90')
+dfMelhores=dFilmes.loc[dFilmes.Avaliação>=90]
+print(dfMelhores)
+
+print('\n5- Classificação dos filmes de acordo com a duração')
+#======================================================================
+# 5- Categorize e rotule os filmes da seguinte forma:
+#              de 0 até média de duração - Abaixo da média
+#              a partir da média até 150 (inclusive) - Regular
+#              acima de 150 - Muito Longo 
+#======================================================================
+dmed=dFilmes.Duração.mean()
+srFxTempoFilmes=pd.cut(dFilmes.Duração,bins=[0,dmed,150,dFilmes.Duração.max()],labels=["Abaixo da média","Regular","Muito Longo"])
+print(srFxTempoFilmes)
+
+print('\n6-Tabela de frequencia dos filmes de acordo com a duração')
+TabFreqTempo=srFxTempoFilmes.value_counts()
+print(TabFreqTempo)
+
+print('\n7- Tabela de Frequencia Percentual (RELATIVA) graficamente')
+TabFreqTempo.plot.pie(title='Tab Freq Percentual Grafica', figsize=(6,6), autopct='%.1f')
+plt.show()
+
+print('\n8- Tabela de Frequencia Percentual (RELATIVA) NUMERICAMENTE')
+tfp = TabFreqTempo / dFilmes.shape[0] * 100
+print(tfp)
+
+print('n9- Por Nota, dos filmes de apenas DRAMA, a quantidade,o max, min de Avaliações ')
+dfDrama=dFilmes.loc[dFilmes.Gênero=="Drama"]
+# print(dfDrama)
+agDrama = dfDrama.groupby('Nota')
+print(agDrama)
+dfResposta = agDrama.Avaliação.agg(['count','max','min'])
+print(dfResposta)
+
