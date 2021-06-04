2b5d08d5788d72c0606784827fe597459d090e2a
carolzehuri <85316647+carolzehuri@users.noreply.github.com>
2021-06-04 17:13:29 -0300

    Update Tarefa3_Grupo6.py

--- Tarefa3_Grupo6.py
+++ Tarefa3_Grupo6.py
@@ -18,61 +18,53 @@ dFilmes=pd.read_excel("Grupo6_excel.xlsx",index_col=1, header=0)
 
 print(dFilmes)
 
-#Para melhorar a visualizaçÃo do Data Frame eliminaremos a coluna que contém a sinopse do filme e a numeração
-print('\n DFilmes sem timeline e Column 1')
-dFilmes.drop(["timeline","Column1"],axis=1,inplace=True)
-print(dFilmes)
-
-print("\n-----------------------------------------------------")
-
-#Substituição de valores
-print('\n1 DFilmes com colunas e index renomeadas em português')
-dFilmes.rename(columns={"year":"Ano","runtime":"Duração","genre": "Gênero","rating":"Avaliação","metascore":"Nota","votes":"Votos","gross":"Faturamento"},inplace=True)
-dFilmes.index.name = 'Nome'
-print(dFilmes)
-
-print("\n-----------------------------------------------------")
-
-print('\n - Preenchendo valores ausentes na Nota e Faturamento \
-      Em nota com a sua media e em Faturamento com o valor mais frequente')
-media = int(dFilmes.Nota.mean())
-dFilmes.fillna({'Nota':media,'Faturamento':dFilmes.Faturamento.mode().loc[0]}, inplace=True)
-print(dFilmes.Nota.append(dFilmes.Faturamento))
-
 print("\n-----------------------------------------------------")
 
 #PERGUNTAS
+print('\n 1- Melhorando o dFilmes:')
+print('\n a) Para melhor visualização, eliminar a coluna sinopse e column1')
+dFilmes.drop(["timeline","Column1"],axis=1,inplace=True)
 
-print('\n1- Criação de categorias dos filmes de acordo com a duração: \
-      de 0 até média de duração (inclusive) - Abaixo da média \
-      a partir da média até 150 (inclusive) - Regular \
-      acima de 150 - Muito Longo ')
+print('\n b) Dfilmes com colunas e index renomeadas em português')
+dFilmes.rename(columns={"year":"Ano","runtime":"Duração","genre": "Gênero","rating":"Avaliação","metascore":"Nota","votes":"Votos","gross":"Faturamento"},inplace=True)
+dFilmes.index.name = 'Nome'
 
+print('\n c) Preeencher os valares distintos na Nota e Faturamento:')
+print('\n    Em nota com a sua média')
+print('\n    Em faturamento com o valor mais frequente')
+media = int(dFilmes.Nota.mean())
+dFilmes.fillna({'Nota':media,'Faturamento':dFilmes.Faturamento.mode().loc[0]}, inplace=True)
+
+print('\n d) Exiba o dFilmes com as alterações feitas')
+print(dFilmes)
+
+print("\n-----------------------------------------------------")
+
+print('\n 2- Criação de categorias dos filmes de acordo com a duração:')
+print('\n de 0 até média de duração (inclusive) - Abaixo da média')
+print('\n a partir da média até 150 (inclusive) - Regular')
+print('\n acima de 150 - Muito Longo ')
+
+print('\n a) Exiba as categorias')
 dmed=dFilmes.Duração.mean()
 srFxTempoFilmes=pd.cut(dFilmes.Duração,bins=[0,dmed,150,dFilmes.Duração.max()],labels=["Abaixo da média","Regular","Muito Longo"])
 print(srFxTempoFilmes)
 
-print("\n-----------------------------------------------------")
-
-print('\n2- Apresente a tabela de frequencia dos filmes de acordo com a duração')
+print('\n b) Apresente a tabela de frequencia dos filmes de acordo com a duração')
 TabFreqTempo=srFxTempoFilmes.value_counts()
 print(TabFreqTempo)
 
-print("\n-----------------------------------------------------")
-
-print('\n3- Apresente a tabela de Frequencia Percentual (RELATIVA) graficamente')
-TabFreqTempo.plot.pie(title='Percentual de filmes de acordo com a faixa de duração', figsize=(6,6), autopct='%.1f')
+print('\n c) Apresente a tabela de Frequencia Percentual (RELATIVA) graficamente')
+TabFreqTempo.plot.pie(title='Tab Freq Percentual Grafica', figsize=(6,6), autopct='%.1f')
 plt.show()
 
-print("\n-----------------------------------------------------")
-
-print('\n4- Apresente a tabela de Frequencia Percentual (RELATIVA) NUMERICAMENTE')
+print('\n d) Apresente a tabela de Frequencia Percentual (RELATIVA) NUMERICAMENTE')
 tfp = TabFreqTempo / dFilmes.shape[0] * 100
 print(tfp)
 
 print("\n-----------------------------------------------------")
 
-print('n5- Por Nota, dos filmes de apenas DRAMA, a quantidade,o max, min e idxmax de Avaliações ')
+print('n 3- Por Nota, dos filmes de apenas DRAMA, a quantidade,o max, min e idxmax de Avaliações ')
 dfDrama=dFilmes.loc[dFilmes.Gênero=="Drama"]
 # print(dfDrama)
 agDrama = dfDrama.groupby('Nota')
@@ -82,11 +74,7 @@ print(dfResposta)
 
 print("\n-----------------------------------------------------")
 
-print('n6 -Tendo como base os filmes com avalição maior ou igual a 90 (dfMelhores), responda:')
-print('\n a) Exiba o nome dos filmes')
-print('\n b) Por genero dos filmes, apresente a quantidade de filmes, tempo médio de duração e nome da melhor avaliação') 
-print('\n c) Crie um dataframe (dfNum) com os valores referentes a avaliação, nota e votos de dfMelhores')
-print('\n d) Exiba a tabela de frequencia resultante do cruzamento de avaliação e nota de dfNum')
+print('n 4 -Tendo como base os filmes com avalição maior ou igual a 90 (dfMelhores), responda:')
 
 print('\n a) Exiba o nome dos filmes')
 dfMelhores=dFilmes.loc[dFilmes.Avaliação>=90]
@@ -109,6 +97,7 @@ print('\n d) Exiba a tabela de frequencia resultante do cruzamento de avaliaçã
 sr = pd.crosstab(dfNum.Avaliação, dfNum.Nota)
 print(sr)
 
+print("\n-----------------------------------------------------")
 #-------
 #Usar em perguntas
 print('\n3- Nome do(s) filme(s) de maior duração')
